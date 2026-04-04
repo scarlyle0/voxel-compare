379ec4b5d73b7dba92e2fd4d1c3546251b4fe679
Sean Carlyle <scarlyle20@gmail.com>
2026-04-04 09:55:04 -0700

    Add instance, adapter, device, queue

--- src/state.rs
+++ src/state.rs
@@ -13,6 +13,37 @@ pub struct State {
 
 impl State {
     pub async fn new(window: Arc<Window>) -> anyhow::Result<State>{
+        let window_size = window.inner_size();
+
+        // Instance is connection to graphics backend, allows request to adapter (choose gpu) and surface
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY
+            flags: Default::default(),
+            memory_budget_thresholds: Default::default(),
+            backend_options: Default::default(),
+            display: None,
+        });
+
+         // Represents the window which images are presented
+        let surface = instance.create_surface(window.clone()).unwrap();
+
+        // Choose GPU
+        let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        }).await?;
+
+        // Use adapter to create device and queue (handle for gpu connection, interface for commands to send to gpu)
+        let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
+            label: None,
+            required_features: wgpu::Features::empty(),
+            experimental_features: wgpu::ExperimentalFeatures::disabled(),
+            required_limits: wgpu::Limits::default(),
+            memory_hints: Default::default(),
+            trace: wgpu::Trace::Off,
+        }).await?;
+
         Ok(Self {
             window
         })
