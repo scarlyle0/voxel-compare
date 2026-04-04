afa02d9c216915c86fb128c8936a6e41593b5867
Sean Carlyle <scarlyle20@gmail.com>
2026-04-03 22:52:24 -0700

    Add device, queue, surface to state

--- src/state.rs
+++ src/state.rs
@@ -3,6 +3,12 @@ use std::sync::Arc;
 
 pub struct State {
     window: Arc<Window>
+
+    surface: wgpu::Surface<'static>, // Represents the window which images are presented
+    device: wgpu::Device, // Open connection to graphics device to create resources
+    queue: wgpu::Queue, // Submit things to run on GPU
+    config: wgpu::SurfaceConfiguration,
+    is_surface_configured: bool,
 }
 
 impl State {
