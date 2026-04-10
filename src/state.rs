30779b95f15c60100140e2c6086ef27f07f1e23c
Sean Carlyle <scarlyle20@gmail.com>
2026-04-09 22:09:10 -0700

    File organization

--- src/state.rs
+++ src/state.rs
@@ -76,53 +76,6 @@ impl Vertex {
     }
 }
 
-#[rustfmt::skip]
-pub const OPENGL_TO_WGPU_MATRIX: glam::Mat4 = glam::Mat4::from_cols(
-    glam::Vec4::new(1.0, 0.0, 0.0, 0.0),
-    glam::Vec4::new(0.0, 1.0, 0.0, 0.0),
-    glam::Vec4::new(0.0, 0.0, 0.5, 0.0),
-    glam::Vec4::new(0.0, 0.0, 0.5, 1.0),
-);
-
-pub struct Camera {
-    pub eye: glam::Vec3,
-    pub target: glam::Vec3,
-    pub up: glam::Vec3,
-    pub aspect: f32,
-    pub fovy: f32,
-    pub znear: f32,
-    pub zfar: f32,
-}
-
-impl Camera {
-    fn build_view_projection_matrix(&self) -> glam::Mat4 {
-        // Moves world to be at position and rotation of camera
-        let view = glam::Mat4::look_at_rh(self.eye, self.target, self.up);
-        // Warps scene to give the effect of depth
-        let proj = glam::Mat4::perspective_rh(self.fovy.to_radians(), self.aspect, self.znear, self.zfar);
-
-        return OPENGL_TO_WGPU_MATRIX * proj * view;
-    }
-}
-
-#[repr(C)]
-#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
-struct CameraUniform {
-    view_proj: [[f32; 4]; 4],
-}
-
-impl CameraUniform {
-    fn new() -> Self {
-        Self {
-            view_proj: glam::Mat4::IDENTITY.to_cols_array_2d(),
-        }
-    }
-
-    fn update_view_proj(&mut self, camera: &Camera) {
-        self.view_proj = camera.build_view_projection_matrix().to_cols_array_2d();
-    }
-}
-
 pub struct State {
     window: Arc<Window>,
 
